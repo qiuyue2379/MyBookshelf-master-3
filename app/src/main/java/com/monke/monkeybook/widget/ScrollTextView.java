fbeeb770b375469fb304bf86dc8c2a557b39d8f2
Administrator <365650856@qq.com>
2018-11-30 12:34:00 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/widget/ScrollTextView.java
+++ app/src/main/java/com/monke/monkeybook/widget/ScrollTextView.java
@@ -47,7 +47,7 @@ public class ScrollTextView extends AppCompatTextView {
         paddingBottom = getTotalPaddingBottom();
 
         //获得控件的实际高度
-        mHeight = getHeight();
+        mHeight =  getMeasuredHeight();
 
         //计算滑动距离的边界
         mOffsetHeight = mLayoutHeight + paddingTop + paddingBottom - mHeight;
@@ -55,12 +55,14 @@ public class ScrollTextView extends AppCompatTextView {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
             //如果是新的按下事件，则对mBottomFlag重新初始化
-            mBottomFlag = false;
+            mBottomFlag = mOffsetHeight <= 0;
+        }
         //如果已经不要这次事件，则传出取消的信号，这里的作用不大
-        if (mBottomFlag)
+        if (mBottomFlag) {
             event.setAction(MotionEvent.ACTION_CANCEL);
+        }
         return super.dispatchTouchEvent(event);
     }
 
