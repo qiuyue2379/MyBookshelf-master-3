f990c44b13526209f1bcce37c77e5d791a8a0c37
Administrator <365650856@qq.com>
2018-12-01 07:50:58 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/presenter/BookListPresenter.java
+++ app/src/main/java/com/monke/monkeybook/presenter/BookListPresenter.java
@@ -1,6 +1,7 @@
 //Copyright (c) 2017. 章钦豪. All rights reserved.
 package com.monke.monkeybook.presenter;
 
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
@@ -22,12 +23,10 @@ import com.monke.monkeybook.model.WebBookModel;
 import com.monke.monkeybook.presenter.contract.BookListContract;
 import com.monke.monkeybook.service.DownloadService;
 import com.monke.monkeybook.utils.NetworkUtil;
-import com.monke.monkeybook.utils.RxUtils;
 import com.trello.rxlifecycle2.android.ActivityEvent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableOnSubscribe;
@@ -85,29 +84,27 @@ public class BookListPresenter extends BasePresenterImpl<BookListContract.View> 
         if (bookShelfBeans == null || mView.getContext() == null) {
             return;
         }
-        Observable<BookShelfBean> data = Observable.fromIterable(new ArrayList<>(bookShelfBeans));
-        Observable<Long> time = Observable.interval(1, TimeUnit.SECONDS);
-        Observable.zip(data, time, (bookShelfBean, aLong) -> {
-            if (!bookShelfBean.getTag().equals(BookShelfBean.LOCAL_TAG) && (!onlyNew || bookShelfBean.getHasUpdate())) {
-                int chapterNum = bookShelfBean.getChapterListSize();
-                for (int start = bookShelfBean.getDurChapter(); start < chapterNum; start++) {
-                    if (!BookshelfHelp.isChapterCached(bookShelfBean.getBookInfoBean(), bookShelfBean.getChapterList(start))
-                            && start < chapterNum - 1) {
-                        DownloadBookBean downloadBook = new DownloadBookBean();
-                        downloadBook.setName(bookShelfBean.getBookInfoBean().getName());
-                        downloadBook.setNoteUrl(bookShelfBean.getNoteUrl());
-                        downloadBook.setCoverUrl(bookShelfBean.getBookInfoBean().getCoverUrl());
-                        downloadBook.setStart(start);
-                        downloadBook.setEnd(downloadNum > 0 ? Math.min(chapterNum - 1, start + downloadNum - 1) : chapterNum - 1);
-                        downloadBook.setFinalDate(System.currentTimeMillis());
-                        DownloadService.addDownload(mView.getContext(), downloadBook);
-                        break;
+        AsyncTask.execute(() -> {
+            for (BookShelfBean bookShelfBean : new ArrayList<>(bookShelfBeans)) {
+                if (!bookShelfBean.getTag().equals(BookShelfBean.LOCAL_TAG) && (!onlyNew || bookShelfBean.getHasUpdate())) {
+                    int chapterNum = bookShelfBean.getChapterListSize();
+                    for (int start = bookShelfBean.getDurChapter(); start < chapterNum; start++) {
+                        if (!BookshelfHelp.isChapterCached(bookShelfBean.getBookInfoBean(), bookShelfBean.getChapterList(start))
+                                && start < chapterNum - 1) {
+                            DownloadBookBean downloadBook = new DownloadBookBean();
+                            downloadBook.setName(bookShelfBean.getBookInfoBean().getName());
+                            downloadBook.setNoteUrl(bookShelfBean.getNoteUrl());
+                            downloadBook.setCoverUrl(bookShelfBean.getBookInfoBean().getCoverUrl());
+                            downloadBook.setStart(start);
+                            downloadBook.setEnd(downloadNum > 0 ? Math.min(chapterNum - 1, start + downloadNum - 1) : chapterNum - 1);
+                            downloadBook.setFinalDate(System.currentTimeMillis());
+                            DownloadService.addDownload(mView.getContext(), downloadBook);
+                            break;
+                        }
                     }
                 }
             }
-            return bookShelfBean;
-        }).compose(RxUtils::toSimpleSingle)
-                .subscribe();
+        });
     }
 
     private void startRefreshBook() {
