60f5a3bd2bc457e5e05414dd24e052775893952c
Administrator <365650856@qq.com>
2019-03-12 18:27:51 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/constant/AppConstant.java
+++ app/src/main/java/com/kunfei/bookshelf/constant/AppConstant.java
@@ -7,10 +7,6 @@ import java.io.File;
 import java.lang.reflect.Type;
 import java.util.Map;
 
-/**
- * Created by newbiechen on 17-4-16.
- */
-
 public class AppConstant {
 
     //Book Date Convert Format
