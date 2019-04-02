13e51c259bbfcd1c02cf927fec703e178f92776f
Administrator <365650856@qq.com>
2019-04-02 19:59:18 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/base/observer/MyObserver.java
+++ app/src/main/java/com/kunfei/bookshelf/base/observer/MyObserver.java
@@ -4,10 +4,15 @@ package com.kunfei.bookshelf.base.observer;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
-public abstract class SimpleObserver<T> implements Observer<T> {
+public abstract class MyObserver<T> implements Observer<T> {
 
     @Override
     public void onSubscribe(Disposable d) {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
 
     }
 
